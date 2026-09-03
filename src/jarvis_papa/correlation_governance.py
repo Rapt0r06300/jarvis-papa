1e9883ba7bc12dc4c6e3cee3c24f5c42f83df9ff
Rapt0r06300 <bermond.florent06@gmail.com>
2026-09-03 16:23:15 +0200

    style(correlation): normalize P7C package import

--- src/jarvis_papa/correlation_governance.py
+++ src/jarvis_papa/correlation_governance.py
@@ -4,7 +4,7 @@ from collections.abc import Mapping
 from dataclasses import dataclass, replace
 from enum import StrEnum
 
-from .situations import ProvenanceRef
+from jarvis_papa.situations import ProvenanceRef
 
 
 _SECRET_KEYS = {
